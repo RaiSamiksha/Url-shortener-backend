c7c8ef16ca8e7cf8f725de0a92d493d75c8e840d
Samiksha Rai <samiksha.rai@kuonitumlare.com>
2024-10-17 18:32:59 +0530

    changes in index file

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const cookieParser = require('cookie-parser');
 const db = require("./src/models");
 const apirouter = require("./src/routes/");
 const logger = require("./logging/index.js");
-// const { restrictToLoggedinUserOnly, checkAuth } = require('./middlewares/auth');
 
 const app = express();
 
@@ -19,18 +18,9 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(cookieParser());
 
-// app.use(function(req, res, next) {
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-//   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-//   res.setHeader('Access-Control-Allow-Credentials', true);
-//   next();
-// });
-
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
 
-// parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.json());
